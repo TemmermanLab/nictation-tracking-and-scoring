fixed some problems with scaling in the worm tracking code and modified the model evaluation code

nictation_scoring/nictation_metrics.py
@@ -82,36 +82,40 @@ def initiation_rate(scores, only_active = True, fps = 5):
         
         for ws in scores:
             
-            if ws[0] == 1:
-                denominator += 1
+            if len(ws) > 0:
             
-            for f in range(1,len(ws)):
-                
-                if ws[f-1] == 1 and ws[f] == 2:
-                    num_transitions += 1
-                
-                if ws[f] == 1:
+                if ws[0] == 1:
                     denominator += 1
+                
+                for f in range(1,len(ws)):
+                    
+                    if ws[f-1] == 1 and ws[f] == 2:
+                        num_transitions += 1
+                    
+                    if ws[f] == 1:
+                        denominator += 1
     else:
         
         for ws in scores:
             
-            if ws[0] == 1 or ws[0] == 0:
-                denominator += 1
+            if len(ws) > 0:
             
-            for f in range(1,len(ws)):
-                
-                if ws[f-1] == 1 and ws[f] == 2:
-                    num_transitions += 1
-                elif ws[f-1] == 1 and ws[f] == 3:
-                    num_transitions += 1
-                elif ws[f-1] == 0 and ws[f] == 2:
-                    num_transitions += 1
-                elif ws[f-1] == 0 and ws[f] == 3:
-                    num_transitions += 1
-                
-                if ws[f] == 1 or ws[f] == 0:
+                if ws[0] == 1 or ws[0] == 0:
                     denominator += 1
+                
+                for f in range(1,len(ws)):
+                    
+                    if ws[f-1] == 1 and ws[f] == 2:
+                        num_transitions += 1
+                    elif ws[f-1] == 1 and ws[f] == 3:
+                        num_transitions += 1
+                    elif ws[f-1] == 0 and ws[f] == 2:
+                        num_transitions += 1
+                    elif ws[f-1] == 0 and ws[f] == 3:
+                        num_transitions += 1
+                    
+                    if ws[f] == 1 or ws[f] == 0:
+                        denominator += 1
                     
     init_rate = num_transitions / denominator
     
@@ -130,38 +134,42 @@ def stopping_rate(scores, only_active = True, fps = 5):
     
     if only_active:
         
-        for ws in scores:
-            
-            if ws[0] == 2:
-                denominator += 1
-            
-            for f in range(1,len(ws)):
+        if len(ws) > 0:
+        
+            for ws in scores:
                 
-                if ws[f-1] == 2 and ws[f] == 1:
-                    num_transitions += 1
-                
-                if ws[f] == 2:
+                if ws[0] == 2:
                     denominator += 1
+                
+                for f in range(1,len(ws)):
+                    
+                    if ws[f-1] == 2 and ws[f] == 1:
+                        num_transitions += 1
+                    
+                    if ws[f] == 2:
+                        denominator += 1
     else:
         
         for ws in scores:
             
-            if ws[0] == 2 or ws[0] == 3:
-                denominator += 1
+            if len(ws) > 0:
             
-            for f in range(1,len(ws)):
-                
-                if ws[f-1] == 2 and ws[f] == 1:
-                    num_transitions += 1
-                elif ws[f-1] == 3 and ws[f] == 1:
-                    num_transitions += 1
-                elif ws[f-1] == 2 and ws[f] == 0:
-                    num_transitions += 1
-                elif ws[f-1] == 3 and ws[f] == 0:
-                    num_transitions += 1
-                
-                if ws[f] == 2 or ws[f] == 3:
+                if ws[0] == 2 or ws[0] == 3:
                     denominator += 1
+                
+                for f in range(1,len(ws)):
+                    
+                    if ws[f-1] == 2 and ws[f] == 1:
+                        num_transitions += 1
+                    elif ws[f-1] == 3 and ws[f] == 1:
+                        num_transitions += 1
+                    elif ws[f-1] == 2 and ws[f] == 0:
+                        num_transitions += 1
+                    elif ws[f-1] == 3 and ws[f] == 0:
+                        num_transitions += 1
+                    
+                    if ws[f] == 2 or ws[f] == 3:
+                        denominator += 1
     
     if denominator == 0:
         stop_rate = np.nan

nictation_scoring/nictation_module.py
@@ -10,7 +10,7 @@ calculating features and nictation metrics are in separate files.
 Issues and improvements:
     
     -Evaluate models only takes training data from one video
-
+    -Currently featured have 16 digits, they could do with fewer
 
 @author: Temmerman Lab
 """
@@ -57,6 +57,7 @@ sys.path.append(home + \
 
 import nictation_features as nf
 import nictation_plotting as nict_plot
+import nictation_metrics as nict_met
 import tracker as trkr
 import data_management_module as dmm
 
@@ -176,26 +177,38 @@ def evaluate_models_accuracy_2(vid_file_train, vid_file_test, **kwargs):
     
     trials = kwargs.get('trials',1)
     
-    model_types = kwargs.get('model_types',['logistic regression','decision tree',
-                   'k nearest neighbors', 'linear discriminant analysis',
-                   'Gaussian naive Bayes', 'support vector machine', 
-                   'random forest', 'neural network'])
+    model_types = kwargs.get('model_types',['logistic regression',
+        'decision tree', 'k nearest neighbors',
+        'linear discriminant analysis', 'Gaussian naive Bayes',
+        'support vector machine', 'random forest', 'neural network'])
     
     scaling_methods = kwargs.get('scaling_methods',['none','min max',
                         'variance','Gaussian','whiten'])
     
     rand_split = kwargs.get('rand_split',False)
     
+    sigmas = kwargs.get('sigmas', np.arange(0,1.5,0.1))
+    
+    fps = kwargs.get('fps', 5)
+    
+    only_active = kwargs.get('only_active', False)
+    
     
     # load features and manual scores for the training and test videos
     df_train = combine_and_prepare_man_scores_and_features(vid_file_train)
     df_test = combine_and_prepare_man_scores_and_features(vid_file_test)
+    man_scores_test_vid = separate_list_by_worm(
+        df_test['manual_behavior_label'], df_test)
     
     # # plot the abundance of worm-frames with each behavior label
     # nict_plot.bar_count(df_train)
     
-    acc = np.empty((len(scaling_methods),trials,len(model_types),3))
-    times = copy.copy(acc)
+    accs = np.empty((len(scaling_methods),trials,len(model_types),len(sigmas)
+                    ,3))
+    times = np.empty((len(scaling_methods),trials,len(model_types),3))
+    NRs = copy.copy(accs)
+    IRs = copy.copy(accs)
+    SRs = copy.copy(accs)
     
     for sm in range(len(scaling_methods)):
         if scaling_methods[sm] != 'none':
@@ -212,8 +225,10 @@ def evaluate_models_accuracy_2(vid_file_train, vid_file_test, **kwargs):
             df_test_scaled = copy.deepcopy(df_test)
         
         for t in range(trials):
-            x_train, x_test, y_train, y_test, wi_train, worminf_test = split(
+            x_train, x_test, y_train, y_test, wi_train, wi_test = split(
                 df_train_scaled, 0.75, rand_split)
+            man_scores_train_train = separate_list_by_worm(y_train, wi_train)
+            man_scores_train_test = separate_list_by_worm(y_test, wi_test)
             
             for mt in range(len(model_types)):
                 print(model_types[mt])
@@ -221,29 +236,93 @@ def evaluate_models_accuracy_2(vid_file_train, vid_file_test, **kwargs):
                 # train on training video and record training and test 
                 # accuracy on this video and elapsed time
                 t0 = time.time()
-                mod, train_acc, train_test_acc, probs, preds = learn_and_predict(
-                    x_train, x_test, y_train, y_test, model_types[mt])
+                categories = np.unique(y_train) # sometimes a label is not
+                # represented in the training set
+                mod, train_acc, train_test_acc, probs, preds = \
+                    learn_and_predict(x_train, x_test, y_train, y_test,
+                                      model_types[mt])
                 train_time = time.time()-t0
+                
+                
+                # make probability infrerence on the training and test 
+                # portions of the training video to be used below
+                probs_train_train = mod.predict_proba(x_train)
+                probs_train_test = mod.predict_proba(x_test)
                 
                 # use the same model to make inferences on another video and
                 # record the elapsed time and accuracy
                 t0 = time.time()
-                preds = mod.predict(
+                probs_test_vid = mod.predict_proba(
                     df_test_scaled[df_test_scaled.columns[3:]])
-                test_test_acc = np.sum(np.array(df_test_scaled[
-                    'manual_behavior_label']) == preds) / len(preds)
                 infr_time = time.time()-t0
                 
+                # cycle through smoothing sigmas
+                for sg in range(len(sigmas)):
+                    
+                    # smooth and inferences and calculate nictation metrics on
+                    # the training set of the training video
+                    probs_smooth = smooth_probabilities(
+                        probs_train_train, sigmas[sg], fps)
+                    preds_smooth = probabilities_to_predictions(probs_smooth,
+                                                                categories)
+                    preds_smooth_list = separate_list_by_worm(
+                        preds_smooth, wi_train)
                 
-                acc[sm,t,mt,0] = train_acc
-                acc[sm,t,mt,1] = train_test_acc
-                acc[sm,t,mt,2] = test_test_acc
+                    NRs[sm,t,mt,sg,0] = nict_met.nictation_ratio(
+                        preds_smooth_list, only_active)
+                    IRs[sm,t,mt,sg,0] = nict_met.initiation_rate(
+                        preds_smooth_list, only_active)
+                    SRs[sm,t,mt,sg,0] = nict_met.stopping_rate(
+                        preds_smooth_list, only_active)
+                    accs[sm,t,mt,sg,0] = compare_scores_list(
+                        man_scores_train_train,preds_smooth_list)
+                    
+                    
+                    # smooth and inferences and calculate nictation metrics on
+                    # the test set of the training video
+                    probs_smooth = smooth_probabilities(
+                        probs_train_test, sigmas[sg], fps)
+                    preds_smooth = probabilities_to_predictions(probs_smooth,
+                                                                categories)
+                    preds_smooth_list = separate_list_by_worm(
+                        preds_smooth, wi_test)
                 
+                    NRs[sm,t,mt,sg,1] = nict_met.nictation_ratio(
+                        preds_smooth_list, only_active)
+                    IRs[sm,t,mt,sg,1] = nict_met.initiation_rate(
+                        preds_smooth_list, only_active)
+                    SRs[sm,t,mt,sg,1] = nict_met.stopping_rate(
+                        preds_smooth_list, only_active)
+                    accs[sm,t,mt,sg,1] = compare_scores_list(
+                        man_scores_train_test, preds_smooth_list)
+                    
+                    
+                    # smooth and inferences and calculate nictation metrics on
+                    # the separate test video
+                    probs_smooth = smooth_probabilities(
+                        probs_test_vid, sigmas[sg], fps)
+                    preds_smooth = probabilities_to_predictions(probs_smooth,
+                                                                categories)
+                    preds_smooth_list = separate_list_by_worm(
+                        preds_smooth, df_test_scaled)
+                
+                    NRs[sm,t,mt,sg,2] = nict_met.nictation_ratio(
+                        preds_smooth_list, only_active)
+                    IRs[sm,t,mt,sg,2] = nict_met.initiation_rate(
+                        preds_smooth_list, only_active)
+                    SRs[sm,t,mt,sg,2] = nict_met.stopping_rate(
+                        preds_smooth_list, only_active)
+                    accs[sm,t,mt,sg,2] = compare_scores_list(
+                        man_scores_test_vid, preds_smooth_list)
+               
+                
+                # training and inference times do not include the smoothing
+                # time, which is basically negligable
                 times[sm,t,mt,0] = train_time
                 times[sm,t,mt,1] = infr_time
                 
     
-    return acc, times
+    return accs, times, NRs, IRs, SRs
                 # heatmap_acc[mt,2*sm:2*sm+2] = [train_acc,test_acc]
 
     # # initialize heatmap for accuracy
@@ -618,9 +697,8 @@ def smooth_probabilities(probabilities, sigma, fps = 5.0):
     if sigma != 0:
         if type(probabilities)==list:
             for w in range(len(probabilities_smooth)):
-                if sigma != 0:
-                    probabilities_smooth[w] = gaussian_filter1d(
-                        probabilities_smooth[w],sigma, axis = 0)
+                probabilities_smooth[w] = gaussian_filter1d(
+                    probabilities_smooth[w],sigma, axis = 0)
         else:
             for behavior in range(np.shape(probabilities_smooth)[1]):
                 probabilities_smooth[:,behavior] = gaussian_filter1d(
@@ -630,10 +708,14 @@ def smooth_probabilities(probabilities, sigma, fps = 5.0):
 
 
 
-def probabilities_to_predictions(probs):
+def probabilities_to_predictions(probs, categories):
+    pred_nums = list()
+    
+    for f in probs: pred_nums.append(np.argmax(f,axis = -1))
+    
     preds = list()
     
-    for w in probs: preds.append(np.argmax(w,axis = -1)-1)
+    for i in pred_nums: preds.append(categories[i])
 
     return preds#np.int8(preds)
 

tracker.py
@@ -69,14 +69,13 @@ class Tracker:
     #         r'de\nictation\mask_R-CNN\Steinernema\20220127_full_frame_Sc_on'+\
     #         r'_udirt_4.pt'
     model_file = os.path.split(__file__)[0] + \
-        r'\dependencies\20220127_full_frame_Sc_on_udirt_4.pt'
-    
-    print('WARNING: Using C. elegans metaparameters')
+        r'\mask_RCNN\Steinernema_mask_RCNN\20220127_full_frame_Sc_on_udirt_4.pt'
+    print('WARNING: Using S. carpocapsae metaparameters')
     
     metaparameters = {
         'centerline_method' : 'ridgeline',
         'centerline_npts' : 50,
-        'max_centerline_length' : 1152, # originally 268 pix
+        'max_centerline_length' : 750, # originally 268 pix, 690 is S. carpo. in um w/ 20% extra
         'max_centerline_angle' : 45,
         'edge_proximity_cutoff' : 10, # pix
         'deformable_model_scale' : 0.5,
@@ -84,8 +83,9 @@ class Tracker:
         'stitching_method' : 'overlap', # originally it was centroid distance
         }
     
-    model_file = os.path.split(__file__)[0] + \
-        r'\dependencies\20220331_full_frame_Ce_on_udirt_2.pt'
+    # model_file = os.path.split(__file__)[0] + \
+    #     r'\dependencies\20220331_full_frame_Ce_on_udirt_2.pt'
+    # print('WARNING: Using C. elegans metaparameters')
     
     model_scale = (960, 1280) # rows, cols
     
@@ -148,6 +148,8 @@ class Tracker:
         if self.segmentation_method == 'intensity':
             self.background = self.get_background(self.vid,
                                             self.parameters['bkgnd_nframes'])
+        else:
+            self.background = None
         self.dimensions = (int(self.vid.get(4)),int(self.vid.get(3)))
         # assumes same aspect ratio
         self.scale_factor = self.model_scale[0]/self.dimensions[0]
@@ -176,9 +178,25 @@ class Tracker:
         
     # wrapper called by tracking GUI
     def save_params(self):
-        save_path = self.vid_path + '\\' \
-            + os.path.splitext(self.vid_name)[0] + '_tracking'
-        dm.save_params_csv(self.parameters, save_path, 'tracking_parameters')
+        
+        
+        # if self.segmentation_method == 'mask_RCNN':
+        #     self.save_path = self.vid_path + '//' + self.vid_name[:-4] + \
+        #         '_mRCNN_tracking'
+        #     self.save_path_troubleshooting = self.save_path + \
+        #         '//mRCNN_troubleshooting'
+        # elif self.segmentation_method == 'intensity':
+        #     self.save_path = self.vid_path + '//' + self.vid_name[:-4] + \
+        #         '_intensity_tracking'
+        #     self.save_path_troubleshooting = self.save_path + \
+        #         '//intensity_troubleshooting'
+        
+        
+        # save_path = self.vid_path + '\\' \
+        #     + os.path.splitext(self.vid_name)[0] + '_tracking'
+        # dm.save_params_csv(self.parameters, save_path, 'tracking_parameters')
+        
+        dm.save_params_csv(self.parameters, self.save_path, 'tracking_parameters')
     
     
     def set_parameters(self,human_checked,bkgnd_meth,bkgnd_nframes,k_sig,
@@ -207,7 +225,7 @@ class Tracker:
         k_size = (round(k_sig*3)*2+1,
                   round(k_sig*3)*2+1)
         area_bnds = np.array(self.parameters['area_bnds']) * \
-            (1/self.parameters['um_per_pix']**2)
+            (1/(self.parameters['um_per_pix']**2))
         max_angle = self.metaparameters['max_centerline_angle']
         max_length = self.metaparameters['max_centerline_length'] * \
             (1/self.parameters['um_per_pix'])
@@ -275,7 +293,8 @@ class Tracker:
         f_thickness = round(2 * font_factor)
         f_color = (0,0,0)
         linewidth = round(1 * font_factor)
-        offset = round(50 * font_factor)
+        linewidth_scale = round(2 * font_factor)
+        offset = round(70 * font_factor)
         
         # create 'final' image showing identified worms
         final_HSV = cv2.cvtColor(cv2.cvtColor(img,cv2.COLOR_GRAY2BGR),
@@ -307,7 +326,7 @@ class Tracker:
         # label the size of all blobs
         for cc_i in cc_is[1:]:
             cc_sz = cc[2][cc_i][4]
-            text = str(cc_sz)
+            text = str(round(cc_sz*(self.parameters['um_per_pix']**2)))
             text_size = cv2.getTextSize(text, f_face, f_scale, f_thickness)[0]
             text_pos = copy.copy(cc[3][cc_i]) 
             text_pos[0] = text_pos[0]-text_size[0]/2 # x centering
@@ -322,15 +341,15 @@ class Tracker:
      
         # show the distance threshold
         if self.parameters['d_thr'] is not None:
-            d_thr = self.parameters['d_thr'] * \
-                (1/self.parameters['um_per_pix'])
-            text = 'd='+str(d_thr)
+            d_thr = np.round(self.parameters['d_thr'] * \
+                (1/self.parameters['um_per_pix'])).astype(np.int32)
+            text = 'd='+str(self.parameters['d_thr'])+' um'
             text_size = cv2.getTextSize(text, f_face, f_scale, f_thickness)[0]
             pt1 = [np.shape(img)[1]-offset,np.shape(img)[0]-offset]
             pt2 = [pt1[0]-d_thr,pt1[1]]
             text_pos = np.array((((pt1[0]+pt2[0])/2,pt1[1])),dtype='uint16')
             text_pos[0] = text_pos[0] - text_size[0]/2 # x centering 
-            text_pos[1] = text_pos[1] - 5 # y offset   
+            text_pos[1] = text_pos[1] - 5 # y offset
             final = cv2.polylines(final, np.array([[pt1,pt2]]), True, 
                                  (0,0,255), linewidth)
             final = cv2.putText(final,text,tuple(text_pos),f_face,f_scale,
@@ -384,8 +403,8 @@ class Tracker:
             
             centroids_frame, centerlines_frame, centerline_flags_frame, \
                     angles_end_1_frame, angles_end_2_frame = \
-                    self.find_worms(img, self.segmentation_method, self.metaparameters, self.parameters, 
-                                    self.model, self.device, self.scale_factor)
+                        self.find_worms(img, self.segmentation_method, self.metaparameters, self.parameters, 
+                                    self.background, self.model, self.device, self.scale_factor)
             
             self.stitch_centroids(centroids_frame, centerlines_frame,
                                   centerline_flags_frame, angles_end_1_frame,